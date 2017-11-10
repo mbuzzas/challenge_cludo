bad2f7aafa28b33ca7b788da8491807a9eab3e57
mbuzzas <mbuzzas3@gmail.com>
2017-11-10 11:10:16 -0600

    final commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function showFilters() {
+	const filters = document.getElementById('filters');
+	if (filters.style.display !== 'none') {
+		filters.style.display = 'none';
+	}
+	else {
+		filters.style.display = 'block';
+	}
+};
+
